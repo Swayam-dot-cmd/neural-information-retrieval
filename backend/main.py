aeb166d50309a21bb0b040482c5b5fc3c9048350
Swayam <swayambhushansingh123@gmail.com>
2026-04-02 07:20:18 +0530

    fix routes and import path

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
-# backend/main.py
-@app.get("/")
-def root():
-    return {"message": "IR system running"}
 from fastapi import FastAPI
-search = None
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+search = None
+
+@app.get("/")
+def root():
+    return {"message": "IR system is running 🚀"}
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,25 +17,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
-    return {"message": "IR system is running 🚀"}
-
-
 @app.get("/search")
 def search_api(query: str, alpha: float = 0.2):
     global search
 
     if search is None:
         print("Importing model lazily...")
-        from model import search as search_func
+        from backend.model import search as search_func
         search = search_func
 
     return {
         "query": query,
         "results": search(query, alpha)
     }
-
-
-
-
