51f04c3bcf62bc27a9eeeb937c304df521d45e9c
Swayam <swayambhushansingh123@gmail.com>
2026-04-05 04:28:59 +0530

    fix import path

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.model import search as search_func
+from model import search as search_func
 
 app = FastAPI()
 
